82b2eeb5494a1c6847e89d93dc6dedc97201fdec
trinebra <157688079+trinebra@users.noreply.github.com>
2024-02-06 14:30:56 +0100

    update

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+function makeRed (color) {
+    document.getElementById('redLight').style.backgroundColor = 'red';
+}
+
+function makeYellow (color) {
+    document.getElementById('yellowLight').style.backgroundColor = 'yellow';
+}
+
+function makeGreen (color) {
+    document.getElementById('greenLight').style.backgroundColor = 'green';
+}
